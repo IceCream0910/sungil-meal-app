cd55236d39a61305499a2981d21cc15a570535ed
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2022-01-02 21:17:08 +0900

    datepicker 적용

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,38 @@ if(!grade || !classNum) {
 }
 */
 
+$.datepicker.setDefaults({
+    dateFormat: 'yy-mm-dd',
+    prevText: '이전 달',
+    nextText: '다음 달',
+    monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+    dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+    dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+    showMonthAfterYear: true,
+    yearSuffix: '년'
+  });
+  
+  $(function () {
+    $('.datepicker').datepicker();
+  });
+
+
+  $( "#datepicker" ).datepicker({
+	onSelect: function (dataTExt, inst) {
+    selectedDate = moment(dataTExt).format('YYYYMMDD');
+    $('#date').html(moment(selectedDate).format('M월 D일'));
+    if ($("#datepickerholder").is(':visible') == false) {
+        $("#datepickerholder").show();
+    } else {
+        $("#datepickerholder").hide();
+    }
+    updateInfo();
+}
+});
+
+
 updateInfo();
 
 function backDate() {
