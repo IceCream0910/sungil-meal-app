509b3b2e0f55316ab6dc5fd14ac8ec0d1bfef029
Icecream0910 <taein2370@naver.com>
2022-09-09 03:58:54 +0900

    UPDATE: offline page

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,7 @@ const CACHE_NAME = "cache-v1";
 // 캐싱할 파일
 const FILES_TO_CACHE = [
     "offline.html",
-    "index_watch.html",
-    "assets/css/style_watch.css",
-    "assets/js/script_watch.js"
+    "assets/js/gsap.min.js"
 ];
 
 // 상술한 파일 캐싱
